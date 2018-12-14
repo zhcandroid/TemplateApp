7b98e71deee3df6bc0a436c4dd2fc884e8b23bef
zhenghuachao <835382519@qq.com>
2018-12-14 14:28:47 +0800

    基础库搭建中···

--- app/src/main/java/com/template/app/arouter/ARouterUriManger.java
+++ app/src/main/java/com/template/app/arouter/ARouterUriManger.java
@@ -3,6 +3,6 @@ package com.template.app.arouter;
 /**
  * 路由path路径管理类
  * */
-public class ARouterUriManger {
-    public static final String AROUTER_TestActivity = "/activity/TestActivity";
+public interface ARouterUriManger {
+   String AROUTER_TestActivity = "/activity/TestActivity";
 }
