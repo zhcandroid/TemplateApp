06e407ad43cb2e53d775de333d94bc3c8ab3ed35
user <email@example.com>
2018-12-19 16:33:16 +0800

    dcj

--- app/src/main/java/com/template/app/fragment/PersionalFragment.java
+++ app/src/main/java/com/template/app/fragment/PersionalFragment.java
@@ -1,16 +1,27 @@
 package com.template.app.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.view.View;
 import android.widget.TextView;
 
 import com.common.baselibrary.base.CommonLazyFragment;
 import com.common.baselibrary.dialog.AppDialog;
 import com.common.baselibrary.dialog.MalertDialog;
+import com.common.baselibrary.utils.ImageUtils;
 import com.common.baselibrary.view.CircleImageView;
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
 import com.template.app.R;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+
+import static android.app.Activity.RESULT_OK;
 
 public class PersionalFragment extends CommonLazyFragment {
     @BindView(R.id.iv_face)
@@ -43,6 +54,9 @@ public class PersionalFragment extends CommonLazyFragment {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.iv_face:
+                PictureSelector.create(PersionalFragment.this)
+                        .openGallery(PictureMimeType.ofImage())
+                        .forResult(PictureConfig.CHOOSE_REQUEST);
                 break;
             case R.id.tv_show_dialog:
                 mDialog = AppDialog.showClearDialog(getActivity(), "show dialog", new View.OnClickListener() {
@@ -52,6 +66,29 @@ public class PersionalFragment extends CommonLazyFragment {
                     }
                 });
                 break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PictureConfig.CHOOSE_REQUEST:
+                    // 图片、视频、音频选择结果回调
+                    List<LocalMedia> selectList = PictureSelector.obtainMultipleResult(data);
+                    // 例如 LocalMedia 里面返回三种path
+                    // 1.media.getPath(); 为原图path
+                    // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true  注意：音视频除外
+                    // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true  注意：音视频除外
+                    // 如果裁剪并压缩了，以取压缩路径为准，因为是先裁剪后压缩的
+                    if (selectList.size()>0) {
+                        LocalMedia media = selectList.get(0);
+                        Bitmap bitmap = ImageUtils.getBitmapByPath(media.getPath());
+                        mIvFace.setImageBitmap(bitmap);
+                    }
+                    break;
+            }
         }
     }
 }
